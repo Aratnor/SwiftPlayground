ce61c8330415ca04718c734b785efe6eea2afb95
Tuna Bolat <tuna.bolat@external.yapikredi.com.tr>
2024-12-28 00:55:49 +0300

    Initial Commit

---
+++ SwiftPlayground/SwiftPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPlaygroundApp.swift
+//  SwiftPlayground
+//
+//  Created by RDC Partner on 28.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftPlaygroundTests/SwiftPlaygroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPlaygroundTests.swift
+//  SwiftPlaygroundTests
+//
+//  Created by RDC Partner on 28.12.2024.
+//
+
+import Testing
+@testable import SwiftPlayground
+
+struct SwiftPlaygroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
